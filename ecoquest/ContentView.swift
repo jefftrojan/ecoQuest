78dc6cecd58dc581430c6d573a8fa7a99a2403bf
jefftrojan <j.dauda@alustudent.com>
2024-02-16 00:01:44 +0300

    Initial Commit

---
+++ ecoquest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ecoquest
+//
+//  Created by macbook on 16/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ecoquest/ecoquestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ecoquestApp.swift
+//  ecoquest
+//
+//  Created by macbook on 16/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct ecoquestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
